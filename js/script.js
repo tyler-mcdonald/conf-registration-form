53eb7ef1ab874db4637c8f813f3ecbe246aab6e0
tyler-mcdonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-06-28 09:29:17 -0500

    Update script.js
    App is in initial working state (to meet minimum requirements) without any known bugs.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 // Basic Info
 const form = document.querySelector(".container form");
 const nameInput = document.querySelector('#name');
@@ -14,16 +9,21 @@ const shirtColorDiv = document.querySelector('#shirt-colors');
 const shirtDesignSelect = document.querySelector('#design');
 // Activities
 const activitiesList = document.querySelector("#activities-box");
+const activitiesLegend = document.querySelector('#activities legend');
 let totalCost = 0;
 // Payments
-const paypalInfo = document.querySelector("#paypal");
-const bitcoinInfo = document.querySelector("#bitcoin");
-const creditCardInfo = document.querySelector("#credit-card");
 const paymentOptions = document.querySelectorAll("#payment option");
 const paymentSelection = document.querySelector("#payment");
 const creditCardPayment = paymentOptions[1];
+const creditCardInfo = document.querySelector("#credit-card");
+const cardNumber = document.querySelector("#cc-num");
+const zipCode = document.querySelector("#zip");
+const cvv = document.querySelector("#cvv");
+const paypalInfo = document.querySelector("#paypal");
+const bitcoinInfo = document.querySelector("#bitcoin");
 
-// Default user views 
+
+// Default form views
 nameInput.focus();
 shirtColorDiv.hidden = true;
 otherJobRole.hidden = true;
@@ -31,7 +31,15 @@ creditCardPayment.selected = true;
 paypalInfo.hidden = true;
 bitcoinInfo.hidden = true;
 
-// Show 'Other Job Role' when user selects dropdown
+// DEBUGGING DEFAULT SETTINGS
+// nameInput.value = 'TEST USER';
+// emailInput.value = 'TEST@EMAIL.COM';
+// // totalCost = 100;
+// cardNumber.value = '1234567890123';
+// zipCode.value = '12345';
+// cvv.value = '123';
+
+// Display 'Other Job Role' when user selects dropdown
 jobRoleSelect.addEventListener('change', e => {
     selection = e.target.value;
     if (selection === 'other') {
@@ -41,7 +49,8 @@ jobRoleSelect.addEventListener('change', e => {
     }
 });
 
-// Show shirt color options by design theme
+// Display shirt color options by design theme
+// refactor to eliminate `colorThemeSelect`
 shirtDesignSelect.addEventListener('change', e => {
     const selection = e.target.value;
     const colorThemeSelect = document.querySelector('#color');
@@ -62,7 +71,7 @@ activitiesList.addEventListener('change', e => {
     const activity = e.target;
     const activitiesTotalCost = document.querySelector('#activities-cost');
     const activityCost = parseInt(activity.dataset.cost);
-    
+
     if (activity.checked) {
         totalCost += activityCost;
     } else if (!activity.checked) {
@@ -73,6 +82,7 @@ activitiesList.addEventListener('change', e => {
 })
 
 // Payment information display
+// Refactor as a function; remove global variables
 paymentSelection.addEventListener('change', e => {
     const selection = e.target.value;
 
@@ -93,7 +103,8 @@ paymentSelection.addEventListener('change', e => {
     }
 });
 
-// Form validation functions -- turn into an object 
+// Form validation functions
+// refactor into an object 
 function nameValidator() {
     const nameValue = nameInput.value;
     const nameIsValid = /\s*[^\s]+\s*/.test(nameValue); // name cannot be blank
@@ -111,48 +122,72 @@ function activityValidator() {
     return activityIsValid;
 }
 
-function paymentValidator() {
-    if (creditCardPayment.selected) {
-        const cardNumber = document.querySelector('#cc-num').value;
-        const numberIsValid = /^[0-9]{13,16}$/g.test(cardNumber);
-
-        const zipCode = document.querySelector('#zip').value;
-        const zipCodeIsValid = /^[0-9]{5}$/g.test(zipCode);
-
-        const cvv = document.querySelector('#cvv').value;
-        const cvvIsValid = /^[0-9]{3}$/g.test(cvv);
-
-        const cardIsValid = (numberIsValid && zipCodeIsValid && cvvIsValid);
-
-        return cardIsValid;
-    } else {
-        return true;
+// Card validation functions
+// add other validations on separate branch
+const cardValidation = {
+    number: () => {
+        if (creditCardPayment.selected) {
+            const numberIsValid = /^[0-9]{13,16}$/g.test(cardNumber.value);
+            return numberIsValid;
+        } else {
+            return true;
+        }
+    },
+    zip: () => {
+        if (creditCardPayment.selected) {
+            const zipCodeIsValid = /^[0-9]{5}$/g.test(zipCode.value);
+            return zipCodeIsValid;
+        } else {
+            return true;
+        }
+    },
+    cvv: () => {
+        if (creditCardPayment.selected) {
+            const cvvIsValid = /^[0-9]{3}$/g.test(cvv.value);
+            return cvvIsValid;
+        } else {
+            return true;
+        }
     }
 }
 
+// function submissionErrors(validationFunction, inputElement, event) {
+//     if (!validationFunction()) {
+//         event.preventDefault();
+//         inputElement.parentElement.classList.add('not-valid');
+//         inputElement.lastElementSibling.style.display = 'inline'; // lastElementSibling causing bug; might change back to nextElementSibling
+//     } else {
+//         inputElement.parentElement.classList.remove("not-valid");
+//         inputElement.parentElement.classList.add("valid");
+//         inputElement.lastElementSibling.style.display = 'none'; // lastElementSibling causing bug; might change back to nextElementSibling
+//     }
+// }
+
+function submissionErrors(validationFunction, inputElement, event) {
+    const parentElement = inputElement.parentElement;
+    if (!validationFunction()) {
+        event.preventDefault();
+        parentElement.classList.add('not-valid');
+        parentElement.lastElementChild.style.display = 'inline'; 
+    } else {
+        parentElement.classList.remove("not-valid");
+        parentElement.classList.add("valid");
+        parentElement.lastElementChild.style.display = 'none'; 
+    }
+}
 
 // Form validations
-// can be refactored using a loop, with these functions stored in an object
+// refactor: add to existing validation object
 form.addEventListener('submit', e => {
     console.clear();
-    
-    if (!nameValidator()) {
-        e.preventDefault();
-        nameInput.parentElement.classList.add('not-valid');
-        console.log('invalid name');
-    }
-    if (!emailValidator()) {
-        e.preventDefault();
-        console.log('invalid email');
-    }
-    if (!activityValidator()) {
-        e.preventDefault();
-        console.log('Must select at least one activity');
-    }
-    if (!paymentValidator()) {
-        e.preventDefault();
-        console.log('Card info invalid');
-    }
+
+    submissionErrors(nameValidator, nameInput, e);
+    submissionErrors(emailValidator, emailInput, e);
+    submissionErrors(activityValidator, activitiesList, e);    
+    submissionErrors(cardValidation.number, cardNumber, e);
+    submissionErrors(cardValidation.zip, zipCode, e);
+    submissionErrors(cardValidation.cvv, cvv, e);
+
 });
 
 //// Accessibility ////
